808e53d6cf59eb36315b0dfeac0de8bbb211bf83
Azeritzh <azhlander@outlook.com>
2019-08-18 17:51:26 +0200

    Designed basket preview popup

--- LegoCase/src/app/app.component.ts
+++ LegoCase/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ListingsPageComponent } from '../app/listings-page/listings-page.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LegoCase';
+  public basketInfo = [
+    {header: 'item1', subheader: 'This is a vase', price: 1200, quantity: 1},
+    {header: 'item2', subheader: 'This is a vase', price: 1002, quantity: 1},
+    {header: 'item1', subheader: 'This is a vase', price: 1200, quantity: 1},
+    {header: 'item2', subheader: 'This is a vase', price: 1002, quantity: 1},
+    {header: 'item1', subheader: 'This is a vase', price: 1200, quantity: 1},
+    {header: 'item2', subheader: 'This is a vase', price: 1002, quantity: 1},
+    {header: 'item1', subheader: 'This is a vase', price: 1200, quantity: 1},
+    {header: 'item2', subheader: 'This is a vase', price: 1002, quantity: 1},
+  ];
+
+  public openBasketPreview = true;
+
+  recieveBasketInfo(items: []) {
+    this.basketInfo = items;
+    this.openBasketPreview = true;
+
+    console.log(this.basketInfo)
+  }
 }
--- LegoCase/src/app/listings-page/listings-page.component.ts
+++ LegoCase/src/app/listings-page/listings-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-listings-page',
@@ -6,21 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listings-page.component.css']
 })
 export class ListingsPageComponent implements OnInit {
+  @Output() basketEvent = new EventEmitter();
 
   constructor() { }
 
   public itemsList1 = [
-    {header: 'item1', subheader: 'This is a vase', price: 1200},
-    {header: 'item2', subheader: 'This is a vase', price: 1002},
-    {header: 'item3', subheader: 'This is a vase', price: 1004},
+    {header: 'item1', subheader: 'This is a vase', price: 1200, quantity: 1},
+    {header: 'item2', subheader: 'This is a vase', price: 1002, quantity: 1},
+    {header: 'item3', subheader: 'This is a vase', price: 1004, quantity: 1},
   ];
 
   public itemsList2 = [
-    {header: 'item4', subheader: 'This is a vase', price: 1200},
-    {header: 'item5', subheader: 'This is a vase', price: 1002},
-    {header: 'item6', subheader: 'This is a vase', price: 1004},
+    {header: 'item4', subheader: 'This is a vase', price: 1200, quantity: 1},
+    {header: 'item5', subheader: 'This is a vase', price: 1002, quantity: 1},
+    {header: 'item6', subheader: 'This is a vase', price: 1004, quantity: 1},
   ];
 
+  public basketitems = [];
+
   ngOnInit() {
+  }
+
+  public addToBasket(item: {}) {
+    this.basketitems.push(item);
+    this.basketEvent.emit(this.basketitems);
+  }
+
+  public removeFromBasket(item: number) {
+
+  }
+
+  public changeItemQuantity(item: number) {
+
   }
 }
